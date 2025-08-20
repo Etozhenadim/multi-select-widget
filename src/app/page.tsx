9da0219762474e5ca1ebe60c638337b3f6a4d195
Nadim <iskakovnadim08@gmail.com>
2025-08-20 16:44:21 +0200

    tw-merge fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 "use client";
-import React, { useState, useMemo } from "react";
-import { SelectedList, Dialog } from "@/components";
+import React, {useState, useMemo} from "react";
+import {SelectedList, Dialog} from "@/components";
 export default function Widget() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   // Simulating a large list of elements
   const elementsList = useMemo(
     () =>
-      Array.from({ length: 300 }, (_, i) => ({
+      Array.from({length: 300}, (_, i) => ({
         id: i + 1,
         label: `Element ${i + 1}`,
       })),
-    [],
+    []
   );
 
   return (
-    <main className="font-sans flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div className="flex flex-col items-start gap-5 bg-white text-black p-5 w-full max-w-[514px] min-h-[772px] rounded-lg shadow-lg">
+    <main className="font-sans flex items-center justify-center h-screen w-full p-8 gap-16">
+      <div className="flex flex-col items-start gap-5 bg-white text-black p-5 size-full max-w-[514px] rounded-lg shadow-lg">
         <h1 className="text-lg font-medium">Select Items</h1>
         <SelectedList />
         <button
--- src/components/dialog/Dialog.tsx
+++ src/components/dialog/Dialog.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from "react";
-import { observer } from "mobx-react-lite";
-import { selectedElementsStore } from "@/stores/selectedElementsStore";
-import { useDebounce } from "@/hooks/useDebounce";
-import { FilterSelect, SearchInput, CurrentSelectedList } from "@/components";
-import { ElementI } from "@/types/elements";
-import { cn } from "@/helpers/utils";
+import React, {useState} from "react";
+import {observer} from "mobx-react-lite";
+import {selectedElementsStore} from "@/stores/selectedElementsStore";
+import {useDebounce} from "@/hooks/useDebounce";
+import {FilterSelect, SearchInput, CurrentSelectedList} from "@/components";
+import {ElementI} from "@/types/elements";
 interface DialogProps {
   setIsDialogOpen: (open: boolean) => void;
   elementsList: ElementI[];
 }
 export const Dialog = observer(
-  ({ setIsDialogOpen, elementsList }: DialogProps) => {
+  ({setIsDialogOpen, elementsList}: DialogProps) => {
     const [selectedElements, setSelectedElements] = useState<ElementI[]>([]);
     const [searchTerm, setSearchTerm] = useState("");
     const [filterValue, setFilterValue] = useState<number | null>(null);
@@ -38,8 +37,10 @@ export const Dialog = observer(
       setIsDialogOpen(false);
     };
 
+    const baseItemClass = "flex items-center p-2 size-full hover:bg-gray-900";
+
     return (
-      <div className="flex flex-col gap-5 relative bg-[#373737] text-white w-full p-4">
+      <div className="flex flex-col gap-5 relative bg-[#373737] text-white size-full p-4">
         <div className="flex items-center justify-between">
           <h2>Select items</h2>
           <button
@@ -53,22 +54,21 @@ export const Dialog = observer(
           <SearchInput value={searchTerm} onChange={setSearchTerm} />
           <FilterSelect value={filterValue} onChange={setFilterValue} />
         </div>
-        <ul className="max-h-[400px] w-full overflow-y-auto border border-gray-300 ">
+        <ul className="flex-1 min-h-0 max-h-[280px] overflow-y-auto w-full border border-gray-300">
           {filteredElements.map((element, index) => {
             const isSelected = selectedElements.some(
-              (item) => item.id === element.id,
+              (item) => item.id === element.id
             );
             const disableOthers = selectedElements.length >= 3 && !isSelected;
             return (
               <li key={index}>
                 <label
                   htmlFor={`checkbox-${index}`}
-                  className={cn(
-                    "flex items-center p-2 w-full hover:bg-gray-900",
+                  className={`${baseItemClass} ${
                     disableOthers
                       ? "cursor-not-allowed opacity-50"
-                      : "cursor-pointer",
-                  )}
+                      : "cursor-pointer"
+                  }`}
                 >
                   <input
                     type="checkbox"
@@ -84,6 +84,7 @@ export const Dialog = observer(
             );
           })}
         </ul>
+
         <CurrentSelectedList
           selectedElements={selectedElements}
           setSelectedElements={setSelectedElements}
@@ -104,5 +105,5 @@ export const Dialog = observer(
         </div>
       </div>
     );
-  },
+  }
 );
